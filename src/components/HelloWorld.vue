a4ab8d2203e12216f69722f510f8e2b194ced408
itliuzheng <itliuzheng@sina.com>
2018-08-30 17:30:46 +0800

    vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,35 +1,97 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <h2 v-on:click="clickUserName">Ecosystem</h2>
-    <el-switch
-      v-model="value2"
-      active-color="#13ce66"
-      inactive-color="#ff4949">
-    </el-switch>
+  <div>
+    <div class="hello">
+      <h1>{{ msg }}</h1>
+      <h2>Essential Links</h2>
+      <h2 v-on:click="clickUserName">Ecosystem</h2>
+      <template v-if="value2">
+        <h1>YES</h1>
+      </template>
+      <template v-else>
+        <h1>NO</h1>
+      </template>
+      <el-switch
+        v-model="value2"
+        active-color="#13ce66"
+        inactive-color="#ff4949">
+      </el-switch>
+    </div>
+    <div id="todo-list-example">
+      <form v-on:submit.prevent="addNewTodo">
+        <label for="new-todo">ADD a Todo</label>
+        <input type="text"
+                id="new-todo"
+               v-model="newTodoText"
+               placeholder="e.g. Feed the cat"
+        >
+        <button>Add</button>
+      </form>
+      <ul>
+        <li
+          is="todo-item"
+          v-for="(todo,index) in todos"
+          :key="todo.id"
+          :title="todo.title"
+          v-on:remove="todos.splice(index,1)"
+        ></li>
+      </ul>
+    </div>
   </div>
+
 </template>
 
 <script>
+  Vue.component('todo-item',{
+    template:'\
+      <li>\
+        {{title}}\
+        <button v-on:click="$emit(\'remove\')">Remove</button>\
+        <button @:click="$emit(remove)">Remove</button>\
+      </li>   \
+     ',
+    props:['title']
+  });
   export default {
     name: 'HelloWorld',
     data () {
       return {
         msg: 'Welcome to Your Vue.js App',
-        value2:true
+        value2:true,
+        newTodoText:'',
+        todos:[
+          {
+            id:1,
+            title:'one'
+          },
+          {
+            id:2,
+            title:'two'
+          },
+          {
+            id:3,
+            title:'three'
+          }
+        ],
+        nextTodoId:4
       }
     },
     methods:{
       clickUserName(){
         console.log(sessionStorage.username);
+      },
+      addNewTodo(){
+        this.todos.push({
+          id:this.nextTodoId++,
+          title:this.newTodoText
+        });
+        this.newTodoText = ''
       }
     }
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
   h1{
     color: blue;
   }
@@ -38,11 +100,11 @@ h1, h2 {
 }
 ul {
   list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
+  padding: 10px 0 0;
+  li{
+    display: block;
+    margin-bottom: 10px;
+  }
 }
 a {
   color: #42b983;
